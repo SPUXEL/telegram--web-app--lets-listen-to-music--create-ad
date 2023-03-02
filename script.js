b5193df35739a15267e47b9095c75c6992b5fa2d
SPUXEL <48756093+SPUXEL@users.noreply.github.com>
2023-03-02 11:54:02 +0300

    tmp1

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// TELEGRAM
+let tg = window.Telegram.WebApp;
+//tg.expand();
+
+tg.MainButton.text = 'Отправить'
+//tg.MainButton.color = '#'
+
+
+// FORM
+let formData = {};
+const form = document.querySelector('form');
+
+form.addEventListener('input', function(event){
+    formData[event.target.name] = event.target.value;
+});
+
+form.addEventListener('submit', async function(event){
+    event.preventDefault();
+    form.classList.add('_sending');
+
+    tg.MainButton.show();
+});
+
+
+Telegram.WebApp.onEvent('mainButtonClicked', function(){
+    tg.sendData(JSON.stringify(formData);
+});
